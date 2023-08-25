dd87610b4ddfa118bc4f077cd6442d7fc41d8a73
Maxime de Visscher <maxime.de.visscher@gmail.com>
2023-08-25 15:37:50 +0200

    Reproduce image bug with expo-updates on Android

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Image } from 'react-native'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Image 1: Working!</Text>
+      <Image source={require('./image1.png')} style={{ width: 100, height: 100, borderWidth: 3, borderColor: 'red' }} />
+      <Text>Image 2: Not visible when expo-update is installed</Text>
+      <Image source={require('./image2.png')} style={{ width: 100, height: 100, borderWidth: 3, borderColor: 'red' }} />
       <StatusBar style="auto" />
     </View>
   );
